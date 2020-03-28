dbaaf6495e91bb77700844f3890691429248ff5f
JohannesFriedrich <johannes.friedrich@posteo.de>
2020-03-28 13:59:13 +0100

    change date for CRAN submission

--- R/hystReet-package.R
+++ R/hystReet-package.R
@@ -4,7 +4,7 @@
 #' Download and analyse environmental data provided by https://hystreet.com/
 #'
 #' \tabular{ll}{Package: \tab hystReet \cr Type: \tab Package \cr Version:
-#' \tab 0.0.1 \cr Date: \tab 2020-04-01 \cr License: \tab GPL-2 \cr }
+#' \tab 0.0.1 \cr Date: \tab 2020-03-28 \cr License: \tab GPL-2 \cr }
 #'
 #' @name hystReet-package
 #'
